50408188e3b528d9013bf8db4048e0498beebda9
foresle <foresle@tutanota.com>
2023-02-26 13:38:14 +0200

    Added sorting by name, creation date, last seen

--- users/helpers.py
+++ users/helpers.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def assemble_mxc_url(mxc_url: str) -> (str, str):
     """
     Return parsed mxc url:
@@ -10,3 +13,28 @@ def assemble_mxc_url(mxc_url: str) -> (str, str):
 
 def get_download_url_for_media(server_name: str, media_id: str, access_token: str) -> str:
     return f'https://{server_name}/_matrix/media/v3/download/{server_name}/{media_id}?access_token={access_token}'
+
+
+def get_last_seen(access_token: str, server_name: str, user_id: str) -> None | int:
+    response = requests.get(url=f'https://{server_name}/_synapse/admin/v2/users/{user_id}/devices',
+                            headers={
+                                'Authorization': f'Bearer {access_token}'
+                            })
+
+    if response.status_code != 200:
+        return None
+
+    response = response.json()
+    if response['total'] == 0:
+        return None
+
+    devices = response['devices']
+
+    # Delete devices without last_seen_ts
+    for device in devices:
+        if device['last_seen_ts'] is None:
+            return None
+
+    devices = sorted(devices, key=lambda k: k['last_seen_ts'], reverse=True)
+
+    return devices[0]['last_seen_ts']
--- users/views.py
+++ users/views.py
@@ -1,16 +1,21 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
 from django.conf import settings
 import requests
 
-from .helpers import assemble_mxc_url, get_download_url_for_media
+from .helpers import assemble_mxc_url, get_download_url_for_media, get_last_seen
 
 
 class UsersView(TemplateView):
     template_name = 'users/users.html'
 
-    def parse_users(self) -> list:
+    def parse_users(self, sort_by: str) -> list:
         users: list = []
-        response = requests.get(f'https://{settings.MATRIX_DOMAIN}/_synapse/admin/v2/users?guests=false', headers={
+
+        request_url = f'https://{settings.MATRIX_DOMAIN}/_synapse/admin/v2/users?guests=false'
+
+        response = requests.get(url=request_url, headers={
             'Authorization': f'Bearer {settings.MATRIX_ADMIN_TOKEN}'
         })
 
@@ -20,19 +25,55 @@ class UsersView(TemplateView):
         users = response.json()['users']
 
         for user in users:
-            if user['avatar_url'] is None:
-                continue
+            # Convert avatars
+            if user['avatar_url'] is not None:
+                mxc_url = assemble_mxc_url(user['avatar_url'])
+                user['avatar_url'] = get_download_url_for_media(
+                    server_name=mxc_url[0],
+                    media_id=mxc_url[1],
+                    access_token=settings.MATRIX_ADMIN_TOKEN
+                )
 
-            mxc_url = assemble_mxc_url(user['avatar_url'])
-            user['avatar_url'] = get_download_url_for_media(
-                server_name=mxc_url[0],
-                media_id=mxc_url[1],
-                access_token=settings.MATRIX_ADMIN_TOKEN
-            )
+            # Get last seen ts
+            if sort_by in ('last_seen', '-last_seen'):
+                last_seen_ts = get_last_seen(
+                    access_token=settings.MATRIX_ADMIN_TOKEN,
+                    server_name=settings.MATRIX_DOMAIN,
+                    user_id=user['name']
+                )
+
+                if last_seen_ts is None:
+                    last_seen_ts = 946684800
+                else:
+                    last_seen_ts = last_seen_ts / 1000
+
+                last_seen_date = datetime.fromtimestamp(last_seen_ts)
+
+                user['last_seen_date'] = last_seen_date
+
+        # Sort by name
+        if sort_by == 'name':
+            users = sorted(users, key=lambda k: k['name'])
+        elif sort_by == '-name':
+            users = sorted(users, key=lambda k: k['name'], reverse=True)
+
+        # Sort by creation date
+        if sort_by == 'creation_date':
+            users = sorted(users, key=lambda k: k['creation_ts'])
+        elif sort_by == '-creation_date':
+            users = sorted(users, key=lambda k: k['creation_ts'], reverse=True)
+
+        # Sort by last seen
+        if sort_by == 'last_seen':
+            users = sorted(users, key=lambda k: k['last_seen_date'])
+        elif sort_by == '-last_seen':
+            users = sorted(users, key=lambda k: k['last_seen_date'], reverse=True)
 
         return users
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['users'] = self.parse_users()
+        sort_by = self.request.GET.get('sort_by', '-date_joined')
+        context['users'] = self.parse_users(sort_by=sort_by)
+        context['sort_by'] = sort_by
         return context
