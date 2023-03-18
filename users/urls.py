257a962ff1656c6d5a6f4f03f64e8aea4c11cf3d
foresle <foresle@tutanota.com>
2023-03-18 16:16:09 +0200

    Added pagination to users page.

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from .views import UsersView, UserView
 app_name = 'users'
 
 urlpatterns = [
-    path('<str:user_id>/', UserView.as_view(), name='detail'),
+    # path('<str:user_id>/', UserView.as_view(), name='detail'),
     path('', UsersView.as_view(), name='list')
 ]
--- users/views.py
+++ users/views.py
@@ -1,48 +1,58 @@
-from datetime import datetime
 from django.core.cache import cache
-from django.views.generic import TemplateView, RedirectView
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import TemplateView, ListView
 from django.conf import settings
 from django.http import Http404
 
-# from users.helpers import load_user_devices
 
-
-class UsersView(TemplateView):
+class UsersView(ListView):
     template_name = 'users/users.html'
+    paginate_by = 10
 
-    def sort_users(self, users: list, sort_by: str) -> list:
-        """
-        Just sort users list.
-        """
+    # def sort_users(self, users: list, sort_by: str) -> list:
+    #     """
+    #     Just sort users list.
+    #     """
+    #
+    #     # Sort by name
+    #     if sort_by == 'name':
+    #         users = sorted(users, key=lambda k: k['name'])
+    #     elif sort_by == '-name':
+    #         users = sorted(users, key=lambda k: k['name'], reverse=True)
+    #
+    #     # Sort by creation date
+    #     if sort_by == 'creation_date':
+    #         users = sorted(users, key=lambda k: k['creation_ts'])
+    #     elif sort_by == '-creation_date':
+    #         users = sorted(users, key=lambda k: k['creation_ts'], reverse=True)
+    #
+    #     # Sort by last seen
+    #     if sort_by == 'last_seen':
+    #         users = sorted(users, key=lambda k: k['last_seen_device']['last_seen_ts'])
+    #     elif sort_by == '-last_seen':
+    #         users = sorted(users, key=lambda k: k['last_seen_device']['last_seen_ts'], reverse=True)
+    #
+    #     return users
 
-        # Sort by name
-        if sort_by == 'name':
-            users = sorted(users, key=lambda k: k['name'])
-        elif sort_by == '-name':
-            users = sorted(users, key=lambda k: k['name'], reverse=True)
-
-        # Sort by creation date
-        if sort_by == 'creation_date':
-            users = sorted(users, key=lambda k: k['creation_ts'])
-        elif sort_by == '-creation_date':
-            users = sorted(users, key=lambda k: k['creation_ts'], reverse=True)
-
-        # Sort by last seen
-        if sort_by == 'last_seen':
-            users = sorted(users, key=lambda k: k['last_seen_device']['last_seen_ts'])
-        elif sort_by == '-last_seen':
-            users = sorted(users, key=lambda k: k['last_seen_device']['last_seen_ts'], reverse=True)
-
-        return users
+    def get_queryset(self) -> tuple:
+        return tuple(cache.get(settings.CACHED_USERS, {}).values())
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        sort_by = self.request.GET.get('sort_by', '-date_joined')
-        context['users'] = self.sort_users(
-            users=cache.get('users', {}).values(),
-            sort_by=sort_by
-        )
-        context['sort_by'] = sort_by
+
+        paginator = Paginator(self.object_list, self.paginate_by)
+        page: str = self.request.GET.get('page')
+
+        try:
+            users = paginator.get_page(page)
+        except PageNotAnInteger:
+            users = paginator.get_page(1)
+        except EmptyPage:
+            users = paginator.get_page(paginator.num_pages)
+
+        context['users'] = users
+        context['cached_users_updated_at'] = cache.get(settings.CACHED_USERS_UPDATED_AT, None)
+        context['total_users'] = paginator.count
 
         context['users_page_active'] = True
 
