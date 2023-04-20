57583d17616247a09493d03afdf2918622b0f7b1
foresle <foresle@tutanota.com>
2023-04-20 19:32:09 +0300

    Added avatar to room page.

--- rooms/helpers.py
+++ rooms/helpers.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-
 import synapse_admin
 from django.conf import settings
 from django.core.cache import cache
 from synapse_admin.base import SynapseException
+
+from project.helpers import assemble_mxc_url, get_download_url_for_media
 
 
 def load_rooms(access_token: str, server_name: str) -> None:
@@ -33,14 +34,35 @@ def load_rooms(access_token: str, server_name: str) -> None:
             'public': room['public']
         }
 
+        # Loading room members
         members: list = []
         try:
             members = room_manager.list_members(room['room_id'])
         except SynapseException:
             pass
 
-        rooms[room['room_id']]['local_members'] = [member for member in members if member.endswith(settings.MATRIX_DOMAIN)]
-        rooms[room['room_id']]['global_members'] = [member for member in members if not member.endswith(settings.MATRIX_DOMAIN)]
+        rooms[room['room_id']]['local_members'] = [member for member in members if
+                                                   member.endswith(settings.MATRIX_DOMAIN)]
+        rooms[room['room_id']]['global_members'] = [member for member in members if
+                                                    not member.endswith(settings.MATRIX_DOMAIN)]
+
+        # Finding the room avatar
+        states = []
+        try:
+            states = room_manager.get_state(room['room_id'])
+        except SynapseException:
+            pass
+
+        states = [state for state in states if state['type'] == 'm.room.avatar']
+        if len(states) == 1:
+            avatar_server_name, avatar_media_id = assemble_mxc_url(states[0]['content']['url'])
+            rooms[room['room_id']]['avatar_url'] = get_download_url_for_media(
+                media_id=avatar_media_id,
+                server_name=avatar_server_name,
+                access_token=settings.MATRIX_ADMIN_TOKEN
+            )
+        else:
+            rooms[room['room_id']]['avatar_url'] = None
 
     cache.set(settings.CACHED_ROOMS_UPDATED_AT, datetime.now(), 60 * 60 * 60 * 24)
     cache.set(settings.CACHED_ROOMS, rooms, 60 * 60 * 60 * 24)
